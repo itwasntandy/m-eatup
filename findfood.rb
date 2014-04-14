8fe80e00a8269628eb9fa2b4b9bea527da3215c2
Andrew Mulholland <anmulholland@expedia.com>
2014-04-14 15:30:04 +0100

    bit a big commit contains proper naming - switching to snake_case for methods and removing Capitalization of variable names. alos a bit of error handling

--- findfood.rb
+++ findfood.rb
@@ -3,6 +3,7 @@ require 'cgi'
 require 'json'
 require 'geokit'
 require 'yelpster'
+require 'uri'
 
 YELP_YWSID = "***REMOVED***"
  
@@ -17,44 +18,53 @@ Yelp.configure(:yws_id => YELP_YWSID,
                :token => YELP_TOKEN,
                :token_secret => YELP_TOKEN_SECRET)
 
-def toRadians(n)
+def to_radians(n)
   r = n * (Math::PI / 180)
   return r
 end
   
-def toDegrees(n)
+def to_degrees(n)
   d = n * (180 / Math::PI)
   return d
 end
 
-
-get '/lookup*' do
-  content_type :json
-  {"params" => CGI::parse(request.query_string)}.to_json
-#  "you asked for #{params} is that what you meant?"
+def find_midpoint(lat1,long1,lat2,long2)
+  dlong = long2 - long1
   
-  first_addr = Geokit::Geocoders::GoogleGeocoder.geocode(params.fetch("address1"))
-  second_addr = Geokit::Geocoders::GoogleGeocoder.geocode(params.fetch("address2"))
-  foodtype = params.fetch("type")
+  bx = Math.cos(lat2) * Math.cos(dlong)
+  by = Math.cos(lat2) * Math.sin(dlong)
+ 
+  latmid = to_degrees(Math.atan2((Math.sin(lat1) + Math.sin(lat2)), Math.sqrt(( Math.cos(lat1)+bx)**2 + by**2)))
+  longmid = to_degrees(long1 + Math.atan2(by, Math.cos(lat1) + bx))
+  return [latmid, longmid]
+end
 
-  lat1 = toRadians(first_addr.lat)
-  lat2 = toRadians(second_addr.lat)
-  lng1 = toRadians(first_addr.lng)
-  lng2 = toRadians(second_addr.lng)
+get '/lookup' do
+  content_type :json
+  "you asked for #{params} is that what you meant?"
+  if !(params.fetch("address1").empty? || params.fetch("address2").empty?)
+    "You haven't added valid addresses"
+  end
+  address1 = URI.unescape(params.fetch("address1"))
+  address2 = URI.unescape(params.fetch("address2"))
+  #set a default foodtype to indian
+  foodtype = "indian"
+  first_addr = Geokit::Geocoders::GoogleGeocoder.geocode(address1)
+  second_addr = Geokit::Geocoders::GoogleGeocoder.geocode(address2)
+  foodtype = URI.unescape(params.fetch("type"))
 
-  dLon = lng2 - lng1
+  lat1 = to_radians(first_addr.lat)
+  lat2 = to_radians(second_addr.lat)
+  long1 = to_radians(first_addr.lng)
+  long2 = to_radians(second_addr.lng)
 
-  Bx = Math.cos(lat2) * Math.cos(dLon)
-  By = Math.cos(lat2) * Math.sin(dLon)
-
-  LatMid = toDegrees(Math.atan2((Math.sin(lat1) + Math.sin(lat2)), Math.sqrt(( Math.cos(lat1)+Bx)**2 + By**2)))
-  LongMid = toDegrees(lng1 + Math.atan2(By, Math.cos(lat1) + Bx))
-
+  midpoint = find_midpoint(lat1,long1,lat2,long2)
+  
   client = Yelp::Client.new
   request = Yelp::V2::Search::Request::GeoPoint.new(
               :term => foodtype,
-              :latitude => LatMid,
-              :longitude => LongMid,
+              :latitude => midpoint[0],
+              :longitude => midpoint[1],
               :limit => 1,
               :category => "food"
                )
