8828f5b008c3db15fa5494cddfd3d58f356a38fc
Andrew Mulholland <andrew@bash.sh>
2014-08-20 21:45:54 -0400

    clean up off old nonsense

--- zulipbot.rb
+++ zulipbot.rb
@@ -85,7 +85,6 @@ class ZulipBot
                 subject = event["message"]["subject"]
                 content = event["message"]["content"]
                 # don't want to search for our bot's name
-                #content = content.sub(/^\@\*\*YelpFoodFinder\*\*\ /, '')
                 content = content.sub(/^<p><span class="user-mention" data-user-email="yelpfoodfinder-bot@students.hackerschool.com">@YelpFoodFinder<\/span>\ /, '')
                 content = content.sub(/<\/p>$/, '')
                 message = food_finder.lookup(content, logger)
